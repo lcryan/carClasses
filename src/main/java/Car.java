57e042b4c865616fbc61ca8b073c69e15e353876
LuciaRyan <lcryan06@gmail.com>
2023-05-10 23:05:23 +0200

    bonus assignment solved

--- src/main/java/Car.java
+++ src/main/java/Car.java
@@ -4,12 +4,19 @@ public class Car {
     int manufacturingYear;
     String color;
     double engineSize;
+    String nameOwner;
+    long driverLicenceNumber;
 
-    public Car(String brandName, String type, int manufacturingYear, String color, double engineSize) {
+    public Car(String brandName, String type, int manufacturingYear, String color, double engineSize, String nameOwner, long driverLicenceNumber) {
         this.brandName = brandName;
         this.type = type;
         this.manufacturingYear = manufacturingYear;
         this.color = color;
         this.engineSize = engineSize;
+        this.nameOwner = nameOwner;
+        this.driverLicenceNumber = driverLicenceNumber;
+    }
+    public void print() {
+        System.out.println("Congratulations! You have chosen a " + brandName + " " + type + " made in " + manufacturingYear + "." + "It is " + color + " and its engine size is " + engineSize + "." + " The previous owner was " + nameOwner + "." + " His driver's licence number is " + driverLicenceNumber + ".");
     }
 }
--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,11 +1,12 @@
 public class Main {
     public static void main(String[] args) {
-        Car carOne = new Car("Volvo", "CX90", 2022, "black", 2.0);
-        Car carTwo = new Car("Fiat", "500", 2019, "green", 1.4);
-        Car carThree = new Car("Toyota", "Aygo", 2016, "blue", 1.2);
+        Car carOne = new Car("Volvo", "CX90", 2022, "black", 2.0, "Klaus", 123675593);
+        Car carTwo = new Car("Fiat", "500", 2019, "green", 1.4, "Five", 342768900);
+        Car carThree = new Car("Toyota", "Aygo", 2016, "blue", 1.2, "Victor", 897309276);
 
-        System.out.println("Congratulations ! You have chosen a " + carOne.brandName + " " + carOne.type + " from " + carOne.manufacturingYear + "." + " It is " + carOne.color + " and its engine size " + carOne.engineSize + ".");
-        System.out.println("Congratulations ! You have chosen a " + carTwo.brandName + " " + carTwo.type + " from " + carTwo.manufacturingYear + "." + " It is " + carTwo.color + " and its engine size " + carTwo.engineSize + ".");
-        System.out.println("Congratulations ! You have chosen a " + carThree.brandName + " " + carThree.type + " from " + carThree.manufacturingYear + "." + " It is " + carThree.color + " and its engine size " + carThree.engineSize + ".");
+        carOne.print();
+        carTwo.print();
+        carThree.print();
+
     }
 }
